a64405f2d008c21cf34051b74d66895d67545fda
Zephyr <phillip.t.mock@gmail.com>
2022-11-18 14:19:45 -0700

    first commit

---
+++ Organization.java
@@ -0,0 +1,43 @@
+package com.aa.act.interview.org;
+
+import java.util.Optional;
+
+public abstract class Organization {
+
+	private Position root;
+	
+	protected Organization() {
+		root = createOrganization();
+	}
+	
+	protected abstract Position createOrganization();
+	
+	/**
+	 * hire the given person as an employee in the position that has that title
+	 * 
+	 * @param person
+	 * @param title
+	 * @return the newly filled position or empty if no position has that title
+	 */
+	public Optional<Position> hire(Name person, String title) {
+		//your code here
+		new Name(title, title);
+		new Position(title);
+
+	
+		return Optional.empty();
+	}
+
+	@Override
+	public String toString() {
+		return printOrganization(root, "");
+	}
+	
+	private String printOrganization(Position pos, String prefix) {
+		StringBuilder sb = new StringBuilder(prefix + "+-" + pos.toString() + "\n");
+		for(Position p : pos.getDirectReports()) {
+			sb.append(printOrganization(p, prefix + "\t"));
+		}
+		return sb.toString();
+	}
+}
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Coding evaluation projects
+# Coding evaluation
+Your goal is to make this project functional by completing the `hire()` method in the `Organization` class. You are free to change existing code or add additonal code, with the exception of `MyOrganization.java`. This file must not be changed.
 
-These projects are intended to help AA interviewers to evaluate the coding skills of job candidates. The projects are all identical in nature, but implemented in different languages.
+## Running the project
+This project should be run on a Java 11 or later JVM. It can be compiled and run in your favorite IDE or by using the command line instructions below.
+
+Compile the project by running:
+
+    javac com/aa/act/interview/org/*.java
+
+The `MyOrganization` class has a `main()` method, so the project can be run with:
+
+    java com.aa.act.interview.org.MyOrganization
+
